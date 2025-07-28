1916be3dc342e13bfc72bd04c51430315ba733c4
anna0121222 <anna012188@gmail.com>
2025-07-28 16:23:29 +0900

    community-list_0728 #3

--- static/js/community-list/community-contents-event.js
+++ static/js/community-list/community-contents-event.js
@@ -1,5 +1,7 @@
 // 상단 게시글 정보 라인 나타나기/숨기기
-const topLine = document.querySelector("div.vue-portal-target");
+const topLine = document.querySelector("div#app div.vue-portal-target");
+
+console.log(topLine);
 
 topLine.style.transition = "opacity 0.3s ease, transform 0.3s ease";
 topLine.style.opacity = 0;
@@ -14,5 +16,57 @@ window.addEventListener("scroll", () => {
     } else {
         topLine.style.visibility = "visible";
         topLine.style.transform = "translateY(-20px)";
+    }
+});
+
+// 인기 고수 추천 이동
+const prevButton = document.querySelector(
+    "section.popular-provider div.slick-slider button.slick-arrow.slick-prev"
+);
+const nextButton = document.querySelector(
+    "section.popular-provider div.slick-slider button.slick-arrow.slick-next"
+);
+const suggestion = document.querySelector(
+    "section.popular-provider div.slick-list div.slick-track"
+);
+let suggestionCount = 0;
+
+console.log(prevButton);
+
+prevButton.addEventListener("click", (e) => {
+    if (suggestionCount > 0) {
+        suggestionCount--;
+        suggestion.style.transform = `translate(-${300 * suggestionCount}px)`;
+        suggestion.style.transition = `transform 0.5s`;
+    }
+
+    if (suggestionCount > 0) {
+        prevButton.style.visibility = "visible";
+    } else {
+        prevButton.style.visibility = "hidden";
+    }
+    if (suggestionCount < 2) {
+        nextButton.style.visibility = "visible";
+    } else {
+        nextButton.style.visibility = "hidden";
+    }
+});
+
+nextButton.addEventListener("click", (e) => {
+    if (suggestionCount < 2) {
+        suggestionCount++;
+        suggestion.style.transform = `translate(-${300 * suggestionCount}px)`;
+        suggestion.style.transition = `transform 0.5s`;
+    }
+
+    if (suggestionCount > 0) {
+        prevButton.style.visibility = "visible";
+    } else {
+        prevButton.style.visibility = "hidden";
+    }
+    if (suggestionCount < 2) {
+        nextButton.style.visibility = "visible";
+    } else {
+        nextButton.style.visibility = "hidden";
     }
 });
