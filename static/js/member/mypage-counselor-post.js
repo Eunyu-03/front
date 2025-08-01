04772d978370220f7c3e2505db48eac1caf14208
sinyoung3095 <sinyoung3095@gmail.com>
2025-08-01 15:40:23 +0900

    close #81 작업 끝

--- static/js/member/mypage-counselor-post.js
+++
@@ -1,6 +0,0 @@
-const view = document.getElementById("view");
-const none = document.getElementById("none");
-console.log(view.innerText);
-view.innerText !== null
-    ? (view.style.display = "block")
-    : (none.style.display = "block");
