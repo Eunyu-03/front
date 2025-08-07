8b8b372f8e496b0e2dd0b32984aa6b28b1538410
Eunyu <wjddmsdb03@gmail.com>
2025-08-07 14:07:10 +0900

    헤더 수정

--- static/js/main-page/header2.js
+++ static/js/main-page/header2.js
@@ -1,6 +1,15 @@
-const mtbutton = document.querySelector("button.log-11");
+const mtbutton = document.querySelector(".mybutton");
 const mydiv = document.querySelector("div.mydiv");
+const profile = document.querySelector(".myprofile");
 
 mtbutton.addEventListener("click", (e) => {
     mydiv.classList.toggle("active");
+    profile.classList.toggle("active");
+});
+
+const alimbutton = document.querySelector("button.alimbutton");
+const alimdiv = document.querySelector("div.alimarea");
+
+alimbutton.addEventListener("click", (e) => {
+    alimdiv.classList.toggle("active");
 });
